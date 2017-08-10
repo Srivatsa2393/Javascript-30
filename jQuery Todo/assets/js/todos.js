326ef9b5fa4c42aad8e68897b66fbd09e9c6737c
Srivatsa S <srivatsashankaran@gmail.com>
2017-08-10 22:28:54 +0530

    Todo list marking completion

---
+++ jQuery Todo/assets/js/todos.js
@@ -0,0 +1,19 @@
+//check off specific todos by clicking
+$('li').click(function(){
+
+    //if li is green turn it black else turn green
+    //if($(this).css("color") === "rgb(128, 128, 128)"){
+ /*        $(this).css({
+        color: 'black',
+        textDecoration: 'none '
+    });
+    }else{
+        $(this).css({
+        color: 'gray',
+        textDecoration: 'line-through'
+    });
+    } */
+
+    $(this).toggleClass("completed");
+
+});    
