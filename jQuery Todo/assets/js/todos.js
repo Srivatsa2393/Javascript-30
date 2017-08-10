12a1c82eccc685b553711f6e57db29f13cde1215
Srivatsa S <srivatsashankaran@gmail.com>
2017-08-10 23:02:29 +0530

    Deleting the todos

--- jQuery Todo/assets/js/todos.js
+++ jQuery Todo/assets/js/todos.js
@@ -17,3 +17,11 @@ $('li').click(function(){
     $(this).toggleClass("completed");
 
 });    
+
+//click on x to delete todo
+$("span").click(function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+})
