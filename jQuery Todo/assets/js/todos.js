d97f4115c853df2551367bbe67fa8b063845d94e
Srivatsa S <srivatsashankaran@gmail.com>
2017-08-10 23:16:57 +0530

    adding todos to the list

--- jQuery Todo/assets/js/todos.js
+++ jQuery Todo/assets/js/todos.js
@@ -1,5 +1,5 @@
 //check off specific todos by clicking
-$('li').click(function(){
+$('ul').on("click", "li",  function(){
 
     //if li is green turn it black else turn green
     //if($(this).css("color") === "rgb(128, 128, 128)"){
@@ -19,9 +19,20 @@ $('li').click(function(){
 });    
 
 //click on x to delete todo
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
     $(this).parent().fadeOut(500, function(){
         $(this).remove();
     });
     event.stopPropagation();
+})
+
+
+//creation of new todos
+$("input[type='text']").keypress(function(event){
+    if (event.which === 13){
+        var todoText = $(this).val();
+        $(this).val('');
+        //create a new li and add to ul
+        $('ul').append("<li><span>X</span> " + todoText + "</li>");
+    }
 })
